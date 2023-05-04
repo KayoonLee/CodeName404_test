57ea85374208e80735d03a8b7d126a65cf32630c
SilvieHong <heh9007@gmail.com>
2023-05-04 16:29:17 +0900

    src > main > java > com > myproject > mycode > model > QnaBoard.java

---
+++ codename404/src/main/java/com/myproject/mycode/model/QnaBoard.java
@@ -0,0 +1,80 @@
+package com.myproject.mycode.model;
+
+import java.util.Date;
+
+public class QnaBoard {
+	private int qna_no;
+	private String qna_nick;
+	private String qna_subject;
+	private String qna_sol;
+	private String qna_content;
+	private Date qna_register;
+	private int qna_readcount;
+	private int qna_singo;
+	private int qna_renum;
+	private int qna_del;
+	
+	public int getQna_no() {
+		return qna_no;
+	}
+	public void setQna_no(int qna_no) {
+		this.qna_no = qna_no;
+	}
+	public String getQna_nick() {
+		return qna_nick;
+	}
+	public void setQna_nick(String qna_nick) {
+		this.qna_nick = qna_nick;
+	}
+	public String getQna_subject() {
+		return qna_subject;
+	}
+	public void setQna_subject(String qna_subject) {
+		this.qna_subject = qna_subject;
+	}
+	public String getQna_sol() {
+		return qna_sol;
+	}
+	public void setQna_sol(String qna_sol) {
+		this.qna_sol = qna_sol;
+	}
+	public String getQna_content() {
+		return qna_content;
+	}
+	public void setQna_content(String qna_content) {
+		this.qna_content = qna_content;
+	}
+	public Date getQna_register() {
+		return qna_register;
+	}
+	public void setQna_register(Date qna_register) {
+		this.qna_register = qna_register;
+	}
+	public int getQna_readcount() {
+		return qna_readcount;
+	}
+	public void setQna_readcount(int qna_readcount) {
+		this.qna_readcount = qna_readcount;
+	}
+	public int getQna_singo() {
+		return qna_singo;
+	}
+	public void setQna_singo(int qna_singo) {
+		this.qna_singo = qna_singo;
+	}
+	public int getQna_renum() {
+		return qna_renum;
+	}
+	public void setQna_renum(int qna_renum) {
+		this.qna_renum = qna_renum;
+	}
+	public int getQna_del() {
+		return qna_del;
+	}
+	public void setQna_del(int qna_del) {
+		this.qna_del = qna_del;
+	}
+	
+	
+
+}
